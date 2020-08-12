e8b7f4aa93d0dd3aaaaa99e21e810f99904a62d9
mangojuicedeli <mgkang0317@gmail.com>
2020-08-12 15:52:17 +0900

    Stomp를 이용한 채팅 서버 개선

--- src/main/java/com/websocket/chat/controller/ChatController.java
+++ src/main/java/com/websocket/chat/controller/ChatController.java
@@ -1,32 +1,24 @@
 package com.websocket.chat.controller;
 
-import java.util.List;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.websocket.chat.model.ChatRoom;
-import com.websocket.chat.service.ChatService;
+import com.websocket.chat.model.ChatMessage;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class ChatController {
 
-	private final ChatService chatService;
+	private final SimpMessageSendingOperations messagingTemplate;
 
-	@PostMapping
-	public ChatRoom createRoom(@RequestParam String name) {
-		return chatService.createRoom(name);
-	}
-
-	@GetMapping
-	public List<ChatRoom> findAllRoom() {
-		return chatService.findAllRoom();
+	@MessageMapping("/chat/message")
+	public void message(ChatMessage message) {
+		if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+			message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+		
+		messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
 	}
 }
